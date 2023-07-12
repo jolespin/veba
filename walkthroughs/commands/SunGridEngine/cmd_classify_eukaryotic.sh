eb9f8cf85df0cea044b3658a8de2e7b0e320b133
Josh L. Espinoza <jol.espinoz@gmail.com>
2023-07-11 17:17:16 -0700

    Removing commands

--- walkthroughs/commands/SunGridEngine/cmd_classify_eukaryotic.sh
+++
@@ -1,6 +0,0 @@
-CODE=0714
-N=classify-eukaryotic
-rm -rf logs/${N}.*
-qsub -M jespinoz@jcvi.org -m e -l centos7 -N ${N} -o logs/${N}.o -e logs/${N}.e -cwd -P ${CODE} "source activate VEBA-classify_env && python ~/Algorithms/Pipelines/veba_pipeline/src/classify-eukaryotic.py -i veba_output/binning/eukaryotic/ -o veba_output/classify/eukaryotic -l 1.0"
-
-
