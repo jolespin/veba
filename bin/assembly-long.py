3ae5189882b1f7008f0e8ebb8b8a6fde6f6a2c66
Allan Phillips <allanjphillips20@gmail.com>
2026-06-12 13:13:21 -0700

    Remove de Bruijn path prefixing from Flye assembly (#208)
    Drop the two prepend_de-bruijn_path.py calls on assembly_graph.gfa
    and assembly_info.txt in get_assembly_cmd(). These only served the
    soon to be deprecated metaCoAG binner. The native Flye outputs are
    still produced and symlinked to the final output, just unprefixed.

--- bin/assembly-long.py
+++ bin/assembly-long.py
@@ -78,42 +78,6 @@ def get_assembly_cmd( input_filepaths, output_filepaths, output_directory, direc
             os.path.join(output_directory, "assembly.fasta"),
             ">",
             os.path.join(output_directory, "assembly.fasta.saf"),
-            
-                "&&",
-                
-            os.environ["prepend_de-bruijn_path.py"],
-            "-i",
-            os.path.join(output_directory, "assembly_graph.gfa"),
-            "--prefix",
-            opts.scaffold_prefix,
-            "-o",
-            os.path.join(output_directory, "assembly_graph.prefixed.gfa"),
-            "--program",
-            "flye",
-            
-                "&&",
-                
-            "mv",
-            os.path.join(output_directory, "assembly_graph.prefixed.gfa"),
-            os.path.join(output_directory, "assembly_graph.gfa"),
-            
-                "&&",
-
-            os.environ["prepend_de-bruijn_path.py"],
-            "-i",
-            os.path.join(output_directory, "assembly_info.txt"),
-            "--prefix",
-            opts.scaffold_prefix,
-            "-o",
-            os.path.join(output_directory, "assembly_info.prefixed.txt"),
-            "--program",
-            "flye",
-            
-                "&&",
-                
-            "mv",
-            os.path.join(output_directory, "assembly_info.prefixed.txt"),
-            os.path.join(output_directory, "assembly_info.txt"),
 
         ]
 
